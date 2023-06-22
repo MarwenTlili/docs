d6e83faf54ae4bb364428923ed1ebf9483f1e2ce
MarwenTlili <46303071+MarwenTlili@users.noreply.github.com>
2023-06-22 18:07:08 +0100

    Update MySQL.sql

--- MySQL.sql
+++ MySQL.sql
@@ -1,13 +1,3 @@
--- MySQL Workbench Forward Engineering
-
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema ems
--- -----------------------------------------------------
-
 -- -----------------------------------------------------
 -- Schema ems
 -- -----------------------------------------------------
@@ -66,7 +56,9 @@ CREATE TABLE IF NOT EXISTS `ems`.`reward` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+-- -----------------------------------------------------
+-- Insert Table `ems`.`employee`
+-- ----------------------------------------------------- 
+INSERT INTO `employee` (`id`, `first_name`, `last_name`, `salary`, `joining_date`, `departement`) 
+VALUES (NULL, 'Kevon', 'O\'Hara', '3700', NOW(), NULL);
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
