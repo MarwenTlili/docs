c7020c858991bbba77a5350ca2cf36a52bfb61a2
MarwenTlili <46303071+MarwenTlili@users.noreply.github.com>
2023-06-22 15:34:10 +0100

    Update MySQL.sql

--- MySQL.sql
+++ MySQL.sql
@@ -1,15 +1,38 @@
--- create employee table OneToMany reward
-CREATE TABLE employee(
-    employee_id int NOT NULL AUTO_INCREMENT IDENTITY PRIMARY KEY,
-    first_name varchar(45) NULL,
-    last_name varchar(45) NULL,
-    salary decimal(18,0) NULL,
-    joining_date datetime(6) DEFAULT NOW(),
-    departement varchar(45) NULL,
-    PRIMARY KEY(employee_id)
-);
+-- -----------------------------------------------------
+-- Schema ems
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `ems` DEFAULT CHARACTER SET utf8mb4 ;
+USE `ems` ;
 
-CREATE TABLE reward(
-	reward_id int NOT NULL,
-	employee_ref_id
-);
+-- -----------------------------------------------------
+-- Table `ems`.`employee`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `ems`.`employee` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(45) NULL,
+  `last_name` VARCHAR(45) NULL,
+  `salary` DECIMAL(18,0) NULL,
+  `joining_date` DATETIME NULL,
+  `departement` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`)
+)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `ems`.`reward`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `ems`.`reward` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `date_reward` DATETIME NULL,
+  `amount` INT NULL,
+  `employee_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_reward_employee_idx` (`employee_id` ASC),
+  CONSTRAINT `fk_reward_employee`
+    FOREIGN KEY (`employee_id`)
+    REFERENCES `ems`.`employee` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+)
+ENGINE = InnoDB;
